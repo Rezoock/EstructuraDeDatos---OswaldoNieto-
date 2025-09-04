526a14b7dd032629a7176d4a92c37f3d840a4dd3
OswaldoNieto <luisdiazj91@gmail.com>
2025-09-03 21:34:15 -0500

    Add files via upload

---
+++ Unidad 1/ProyectoCircunferencia/ProyectoCircunferencia/Circunferencia.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCircunferencia
+{
+    internal class Circunferencia
+    {
+        private double dblRadio;
+        public double Radio
+        {
+            get { return dblRadio; }
+            set { dblRadio = value; }
+        }
+       
+        public double CalcularCircunferencia()
+        {
+            return (Math.PI * 2)* dblRadio ;
+        }
+    }
+}
---
+++ Unidad 1/ProyectoCircunferencia/ProyectoCircunferencia/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more informationMI
+using ProyectoCircunferencia;
+
+Circunferencia miCircunferencia = new Circunferencia();
+
+Console.WriteLine("Ingrese el radio de la circunferencia");
+miCircunferencia.Radio = double.Parse(Console.ReadLine());  
+Console.WriteLine("El area de la circunferencia es: " + miCircunferencia.CalcularCircunferencia());
+Console.ReadKey();
